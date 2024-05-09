96d0e12ecf43c47f42addc026f48ce6f8e6b1da1
梁雄 <lxrj2008@163.com>
2024-05-09 12:55:39 +0800

    循环对话

--- langchaindemo/cli.py
+++ langchaindemo/cli.py
@@ -1,12 +1,16 @@
 from Conversation import Conversation
+from datetime import datetime
 
 def chat_loop():
     try:
         conversation = Conversation('device01')
         while True:
             user_input = input("\nYou: ")
+            current_time = datetime.now().strftime("%H:%M:%S")  
+            print(f"[{current_time}] you: {user_input}")  
             res = conversation.ask(question=f"{user_input}")
-            print("\nBot：", res.content)
+            current_time = datetime.now().strftime("%H:%M:%S")  
+            print(f"[{current_time}] Bot: {res.content}")  
     
     except Exception as e:
         print("An error occurred:", e)
